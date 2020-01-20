312d6a16f4bd49ba0645429aaf63e3cb1958e841
“babbleman” <keizaburo.takashiba@force-corp.co.jp>
2020-01-20 23:12:56 +0900

    hello

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session=require('express-session')
 var MongoStore = require('connect-mongo')(session);
 
 var indexRouter = require('./routes/index');
